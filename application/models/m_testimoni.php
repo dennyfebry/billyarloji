3f7d5b766e81a8c1026c6185840afd71a15c4ef9
Denny Febry Prayogo <dennyfebry.p@gmail.com>
2020-03-24 22:24:40 +0700

    model

---
+++ application/models/m_testimoni.php
@@ -0,0 +1,16 @@
+<?php
+defined('BASEPATH') or exit('No direct script access allowed');
+
+class M_slider extends CI_Model
+{
+    // public function check_login($table, $where)
+    // {
+    //     return $this->db->get_where($table, $where);
+    // }
+
+    // function last_login($table, $where, $data)
+    // {
+    //     $this->db->where($where);
+    //     $this->db->update($table, $data);
+    // }
+}
