b1684e5f778a6a6c1e4edb8737e6a6ce860aea9f
Denny Febry Prayogo <dennyfebry.p@gmail.com>
2020-04-03 07:37:28 +0700

    sql

--- billyarloji.sql
+++ billyarloji.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Mar 23, 2020 at 10:11 AM
+-- Generation Time: Apr 03, 2020 at 02:36 AM
 -- Server version: 10.4.11-MariaDB
 -- PHP Version: 7.2.28
 
@@ -42,8 +42,60 @@ CREATE TABLE `tb_admin` (
 --
 
 INSERT INTO `tb_admin` (`id`, `name`, `username`, `password`, `last_login`, `role`) VALUES
-(1, 'Super Admin', 'super.admin', 'cf18e01abec026e46d848b53d6084a6944c206ce', '2020-03-22 23:13:18', 1),
+(1, 'Super Admin', 'super.admin', 'cf18e01abec026e46d848b53d6084a6944c206ce', '2020-04-01 16:28:34', 1),
 (2, 'Admin', 'admin', 'd033e22ae348aeb5660fc2140aec35850c4da997', '2020-03-22 23:13:31', 2);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tb_product`
+--
+
+CREATE TABLE `tb_product` (
+  `id` int(11) NOT NULL,
+  `name` int(50) NOT NULL,
+  `description` varchar(250) NOT NULL,
+  `price` decimal(10,0) NOT NULL,
+  `discount` decimal(10,0) NOT NULL,
+  `images` varchar(100) NOT NULL,
+  `created_date` datetime NOT NULL,
+  `created_by` int(11) NOT NULL,
+  `updated_date` datetime NOT NULL,
+  `updated_by` int(11) NOT NULL,
+  `status` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tb_slider`
+--
+
+CREATE TABLE `tb_slider` (
+  `id` int(11) NOT NULL,
+  `title` varchar(50) NOT NULL,
+  `description` varchar(100) NOT NULL,
+  `images` varchar(100) NOT NULL,
+  `updated_date` datetime NOT NULL,
+  `updated_by` int(11) NOT NULL,
+  `status` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tb_testimoni`
+--
+
+CREATE TABLE `tb_testimoni` (
+  `id` int(11) NOT NULL,
+  `title` varchar(50) NOT NULL,
+  `description` varchar(500) NOT NULL,
+  `created_date` datetime NOT NULL,
+  `created_by` int(11) NOT NULL,
+  `updated_date` datetime NOT NULL,
+  `updated_by` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Indexes for dumped tables
@@ -56,6 +108,24 @@ ALTER TABLE `tb_admin`
   ADD PRIMARY KEY (`id`);
 
 --
+-- Indexes for table `tb_product`
+--
+ALTER TABLE `tb_product`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `tb_slider`
+--
+ALTER TABLE `tb_slider`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `tb_testimoni`
+--
+ALTER TABLE `tb_testimoni`
+  ADD PRIMARY KEY (`id`);
+
+--
 -- AUTO_INCREMENT for dumped tables
 --
 
@@ -64,6 +134,24 @@ ALTER TABLE `tb_admin`
 --
 ALTER TABLE `tb_admin`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT for table `tb_product`
+--
+ALTER TABLE `tb_product`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `tb_slider`
+--
+ALTER TABLE `tb_slider`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `tb_testimoni`
+--
+ALTER TABLE `tb_testimoni`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
