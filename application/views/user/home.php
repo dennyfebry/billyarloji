b1e130324ef301bd44f144ced930d97ece2c50b6
mfawwazi <mfawwaz13@gmail.com>
2020-04-24 16:00:04 +0700

    Testimoni

--- application/views/user/home.php
+++ application/views/user/home.php
@@ -27,10 +27,30 @@ defined('BASEPATH') or exit('No direct script access allowed');
     <!-- End Slider Section -->
 
     <!-- Start Testimoni Section -->
-    <div class='row p-1' style='height: 200px'>
-        <div class='productColumn-0 productColumn-05-md productColumn-05-lg'></div>
-        <div class='productColumn-10 productColumn-9-md productColumn-9-lg testimoni'></div>
-        <div class='productColumn-0 productColumn-05-md productColumn-05-lg'></div>
+    <div class='row testimoni p-1'>
+        <div class='row testimoni-title'>
+            <span class="brown-text">TESTI</span>
+            <span>MONI</span>
+        </div>
+        <div class='row testimoni-item'>
+            <div class="swiper-container testimoni-container">
+                <div data-swiper-parallax="-23%"></div>
+                <div class="swiper-wrapper testimoni-wrapper">
+                    <?php for ($i=0; $i <10 ; $i++) { ?>
+                    <div class="swiper-slide testimoni-slide row">
+                        <div class="title brown-text" data-swiper-parallax="-300">Testimoni <?php echo $i+1 ?></div>
+                        <div class="text" data-swiper-parallax="-100">
+                            <p> &#65282;Barang mantab, pengiriman oke, tokcer deh! Barang mantab, pengiriman oke,
+                                tokcer deh! Barang mantab, pengiriman oke, tokcer deh!
+                                &#65282;</p>
+                        </div>
+                    </div>
+                    <?php } ?>
+                </div>
+                <div class="swiper-button-prev swiper-button-white testimoni-prev"></div>
+                <div class="swiper-button-next swiper-button-white testimoni-next"></div>
+            </div>
+        </div>
     </div>
     <!-- End Testimoni Section -->
 
@@ -40,18 +60,18 @@ defined('BASEPATH') or exit('No direct script access allowed');
             <span class="brown-text pr-03 float-right">INSTAGRAM</span>
             <span class="float-right">FEEDS</span>
         </div>
-        <div class='row feeds-item' style='width: 100%'>
-            <div class="row">
-                <div class="swiper-container feeds-container">
-                    <div class="swiper-wrapper feeds-wrapper">
-                        <?php for ($i=0; $i <10 ; $i++) { ?>
-                        <div class="swiper-slide feeds-slide">
-                            <img class='feeds-image' src="<?php echo base_url(); ?>upload/product/2.jpg">
-                        </div>
-                        <?php } ?>
+        <div class='row feeds-item'>
+            <!-- <div class="row"> -->
+            <div class="swiper-container feeds-container">
+                <div class="swiper-wrapper feeds-wrapper">
+                    <?php for ($i=0; $i <10 ; $i++) { ?>
+                    <div class="swiper-slide feeds-slide">
+                        <img class='feeds-image' src="<?php echo base_url(); ?>upload/product/2.jpg">
                     </div>
+                    <?php } ?>
                 </div>
             </div>
+            <!-- </div> -->
         </div>
     </div>
     <!-- End Feeds Section -->
@@ -94,6 +114,7 @@ defined('BASEPATH') or exit('No direct script access allowed');
 <script src="<?php echo base_url(); ?>assets/js/swiper.min.js"></script>
 
 <script>
+// Vertical Swiper JS
 var verticalSwiper = new Swiper('.swiper-container.vertical-container', {
     direction: 'vertical',
     loop: true,
@@ -107,6 +128,26 @@ var verticalSwiper = new Swiper('.swiper-container.vertical-container', {
     },
 });
 
+// Testimoni Swiper JS
+var testimoniSwiper = new Swiper('.swiper-container.testimoni-container', {
+    speed: 600,
+    parallax: true,
+    loop: true,
+    pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+    },
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+    autoplay: {
+        delay: 5000,
+        disableOnInteraction: false,
+    },
+});
+
+// Instagram Feeds Swiper JS
 var feedsSwiper = new Swiper('.swiper-container.feeds-container', {
     slidesPerView: 4,
     spaceBetween: 10,
--- application/views/user/slider.php
+++ application/views/user/slider.php
@@ -2,13 +2,15 @@
 defined('BASEPATH') or exit('No direct script access allowed');
 ?>
 
+<link rel="stylesheet" type="text/css" href="<?php echo base_url(); ?>assets/css/slider.css" />
+
 <div>
     <div class="slider-wrapper">
         <div class="slider-border">
             <?php for ($i=1; $i <= 5; $i++): ?>
             <div class="mySlides">
                 <div class="numbertext"><?php echo $i; ?> / 5</div>
-                <img src="<?php echo base_url(); ?>assets/images/products/jam.jpg" style="width:100%">
+                <img src="<?php echo base_url(); ?>upload/product/<?php echo $i ?>.jpg" style="width:100%">
             </div>
             <?php endfor; ?>
         </div>
@@ -19,7 +21,7 @@ defined('BASEPATH') or exit('No direct script access allowed');
         <div class="slider-row">
             <?php for ($i=1; $i <= 5; $i++): ?>
             <div class="slider-column">
-                <img class="demo cursor" src="<?php echo base_url(); ?>assets/images/products/jam.jpg"
+                <img class="demo cursor" src="<?php echo base_url(); ?>upload/product/<?php echo $i ?>.jpg"
                     style="width:100%" onclick="currentSlide(<?php echo $i ?>);" alt="Product">
             </div>
             <?php endfor; ?>
@@ -27,5 +29,4 @@ defined('BASEPATH') or exit('No direct script access allowed');
     </div>
 </div>
 
-<link rel="stylesheet" type="text/css" href="<?php echo base_url(); ?>assets/css/slider.css" />
 <script type="text/javascript" src="<?php echo base_url(); ?>assets/js/slider.js"></script>
