125fd0110126892619257c3871e5f1cf4e6bd13f
mfawwazi <mfawwaz13@gmail.com>
2020-04-29 14:50:05 +0700

    Search Category Attribute

--- application/controllers/user/C_product.php
+++ application/controllers/user/C_product.php
@@ -19,8 +19,8 @@ class C_product extends CI_Controller
     {
         $category = $this->ctg->getById($category_id);
         $brand = $this->brn->getById($brand_id);
-        $data['product'] = $this->prd->getProduct($category_id, $brand_id);
 
+        $data['product'] = $this->prd->getProduct($category_id, $brand_id);
         $data['category'] = $category->category;
         $data['brandName'] = explode(" ", $brand->brand);
         $data['titlepage'] = "Product - " . $brand->brand;
@@ -31,7 +31,7 @@ class C_product extends CI_Controller
     public function search($searchValue)
     {
         $data['product'] = $this->prd->searchProduct(urldecode($searchValue));
-
+        $data['category'] = '';
         $data['searchValue'] = urldecode($searchValue);
         $data['titlepage'] = "Search for product";
         $data['page'] = "product";
